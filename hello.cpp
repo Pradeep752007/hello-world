c840020335cef9c9de0c462f8c3ff5fb7e71f834
Pradeep752007 <pradeep752007@gmail.com>
2025-12-29 20:08:06 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 int main(){
   cout<<"Hello World"<<endl;
-  cout<<"This program is just to check how c++ works on github platform"<<endl;
+  cout<<"This program is designed just to check how c++ works on github platform"<<endl;
   return 0;
 }
